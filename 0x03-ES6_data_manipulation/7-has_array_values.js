cc200bd183c5cb6bcf04f305a74bdf52025281fd
malazhatim <malaz.hatim@gmail.com>
2024-07-29 18:38:04 +0000

    7

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,7 @@
+/**
+ * Checks if a set contains each element in an array.
+ * @returns {Boolean}
+ */
+export default function hasValuesFromArray(set, array) {
+  return array.every((value) => set.has(value));
+}
